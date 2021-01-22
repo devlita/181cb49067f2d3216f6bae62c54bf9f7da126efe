6b322579caf63963c78756eb10bb772991f58bc7
devlita <karmelitadevni@gmail.com>
2021-01-22 22:25:57 +0800

    lunch/dinner redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LDbar from './components/LDbar';
 import Lunch from './contents/Lunch';
@@ -11,12 +11,20 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Calendar />
       <Router>
         <LDbar />
         <Switch>
-            <Route exact path="/lunch" component={Lunch} />
-            <Route path="/dinner" component={Dinner} />
+          <Route
+            exact
+            path="/181cb49067f2d3216f6bae62c54bf9f7da126efe"
+            render={() => {
+                return (
+                  <Redirect to="/lunch" />
+                )
+            }}
+          />
+          <Route exact path="/lunch" component={Lunch} />
+          <Route exact path="/dinner" component={Dinner} />
         </Switch>
       </Router>
     </div>
