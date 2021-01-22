17cbda90017621592200fbb1823eb16b3cbfefa1
devlita <karmelitadevni@gmail.com>
2021-01-22 21:13:04 +0800

    bottom modal base

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,44 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="p-2 navbar-light bg-light">
+            <div>
+                <nav className="p-2 navbar-light bg-light">
                     <div class="navbar-brand d-flex" href="#">
                         <span class="material-icons">arrow_back</span>
                         <div>
                             <p class="fs-7 text-muted">ALAMAT PENGANTARAN</p>
-                            <div class="d-flex">
-                                <p class="m-0 fs-6">Tokopedia Tower</p>
+                            <button 
+                                type="button" 
+                                class="btn border-0 .bg-transparent fs-6" 
+                                data-toggle="modal" 
+                                data-target="#bottom_modal"
+                            >
+                                Tokopedia Tower 
                                 <span class="material-icons redL">expand_more</span>
+                            </button>
+                        </div>
+                    </div>
+                </nav>
+                <div class="modal bottom fade" id="bottom_modal" tabindex="-1" role="dialog" aria-labelledby="bottom_modal">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="m-2 d-flex justify-content-end">
+                                <button type="button" class="btn close" data-dismiss="modal" aria-label="Close" >
+                                    <span aria-hidden="true">&times;</span>
+                                </button>    
+                            </div>
+                            <div class="modal-header">
+                                <h5 class="modal-title">Cek makanan yang tersedia di lokasi kamu!</h5>
+                            </div>
+                            <div class="modal-body">
+                                <div>
+                                Kulina
+                                </div>
                             </div>
                         </div>
                     </div>
-            </nav>
+                </div>
+            </div>
             )
         }
     }
